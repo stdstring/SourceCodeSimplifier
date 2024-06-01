bfab9667381e6cee72128b21af4aac2c3acd3923
stdstring <std_string@mail.ru>
2024-06-01 21:55:27 +0500

    added projects for simplifier app and tests on it

---
+++ SourceCodeSimplifier/SourceCodeSimplifierApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SourceCodeSimplifierApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
