c76ddefe4302388ae00b6324b0ca0f999f8ebd1b
stdstring <std_string@mail.ru>
2024-06-02 21:13:51 +0500

    added functionality for configure of app + tests on it

---
+++ SourceCodeSimplifier/SourceCodeSimplifierApp/Processors/SourceProcessorFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace SourceCodeSimplifierApp.Processors
+{
+    internal static class SourceProcessorFactory
+    {
+        public static Boolean IsSupportedSource(String source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
